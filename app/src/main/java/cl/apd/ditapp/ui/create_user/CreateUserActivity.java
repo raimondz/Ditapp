d3743705004df7910bbc0dfbfabbc5df76a1f2bb
Raimundo <arcania.raimond@gmail.com>
2015-10-03 18:13:15 -0300

    Added validation on create user

--- app/src/main/java/cl/apd/ditapp/ui/create_user/CreateUserActivity.java
+++ app/src/main/java/cl/apd/ditapp/ui/create_user/CreateUserActivity.java
@@ -1,19 +1,18 @@
 package cl.apd.ditapp.ui.create_user;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import cl.apd.ditapp.R;
+import cl.apd.ditapp.util.ValidationUtil;
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 /**
  * Created by Raimondz on 03-10-15.
@@ -24,20 +23,18 @@ public class CreateUserActivity extends AppCompatActivity{
 
     final static private int RESULT_BARCODE=0;
 
-    private EditText email;
-    private EditText phone;
+    private EditText et_email;
+    private EditText et_phone;
 
-    private TextView rut;
-
-    private String result;
+    private TextView et_rut;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
-        email= (EditText) findViewById(R.id.et_email);
-        phone= (EditText) findViewById(R.id.et_phone);
-        rut= (TextView) findViewById(R.id.tv_rut);
+        et_email = (EditText) findViewById(R.id.et_email);
+        et_phone = (EditText) findViewById(R.id.et_phone);
+        et_rut = (TextView) findViewById(R.id.tv_rut);
     }
 
     public void onClickScan(View v)
@@ -51,6 +48,40 @@ public class CreateUserActivity extends AppCompatActivity{
 
     public void onCreateClick(View v)
     {
+        SweetAlertDialog dialog=new SweetAlertDialog(this);
+        dialog.setTitleText(getString(R.string.create_user_error_title));
+        String email=et_email.getText().toString();
+        String phone=et_phone.getText().toString();
+        String rut=et_rut.getText().toString();
+
+        if(email.isEmpty())
+        {
+            dialog.setContentText(getString(R.string.create_user_error_no_email));
+            dialog.show();
+            return;
+        }
+
+        if(!ValidationUtil.emailValidation(email))
+        {
+            dialog.setContentText(getString(R.string.create_user_error_no_valid_email));
+            dialog.show();
+            return;
+        }
+
+        if(phone.isEmpty())
+        {
+            dialog.setContentText(getString(R.string.create_user_error_no_phone));
+            dialog.show();
+            return;
+        }
+
+        if(rut.isEmpty())
+        {
+            dialog.setContentText(getString(R.string.create_user_error_no_rut));
+            dialog.show();
+            return;
+        }
+
         //TODO realizar registro
     }
 
@@ -72,13 +103,13 @@ public class CreateUserActivity extends AppCompatActivity{
                     Integer run_index=scanned_text.indexOf("RUN=");
                     scanned_text=scanned_text.substring(run_index+4,run_index+4+10);
                     //Toast.makeText(this,,Toast.LENGTH_SHORT).show();
-                    rut.setText(scanned_text);
+                    et_rut.setText(scanned_text);
                 }
                 if(result.getFormatName().equals("PDF_417")) {
                     String scanned_text = result.getContents();
                     scanned_text = scanned_text.substring(0, 9);
                     scanned_text=scanned_text.substring(0,8)+"-"+scanned_text.substring(8,9);
-                    rut.setText(scanned_text);
+                    et_rut.setText(scanned_text);
                 }
                 break;
             default:
---
+++ app/src/main/java/cl/apd/ditapp/util/ValidationUtil.java
@@ -0,0 +1,22 @@
+package cl.apd.ditapp.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by Raimondz on 03-10-15.
+ */
+public class ValidationUtil {
+
+    final static private String email_pattern ="(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+    static public Boolean emailValidation(String email){
+        Pattern p= Pattern.compile(email_pattern);
+        Matcher m=p.matcher(email);
+        while(m.find())
+        {
+            return true;
+        }
+        return false;
+    }
+}
