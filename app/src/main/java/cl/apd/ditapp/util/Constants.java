3f0d5ff1342829b4f7c36c89b7b7a26cb0f76da6
Raimundo <arcania.raimond@gmail.com>
2015-10-04 03:36:04 -0300

    Changed GSM id

--- app/src/main/java/cl/apd/ditapp/util/Constants.java
+++ app/src/main/java/cl/apd/ditapp/util/Constants.java
@@ -9,7 +9,7 @@ public class Constants {
     public static final String TAG = "APD";
 
     // Google gcm
-    public static final String SENDER_ID = "886274646504";
+    public static final String SENDER_ID = "993329847213";
     public static final String REGISTRATION_COMPLETE = "registrationComplete";
     public static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
 
